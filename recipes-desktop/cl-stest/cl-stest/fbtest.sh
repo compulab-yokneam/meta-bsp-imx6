02ee80eb3e560ee74803b8908575a98bdadf0b87
Valentin Raevsky <valentin@compulab.co.il>
2023-05-15 21:15:12 +0300

    recipes-desktop: cl-stest: Update fbtest.sh
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- recipes-desktop/cl-stest/cl-stest/fbtest.sh
+++ recipes-desktop/cl-stest/cl-stest/fbtest.sh
@@ -1,11 +1,18 @@
 #!/bin/bash -x
 
 pkill -9 gst-launch
+
+for blank in 1 0;do
+    sleep .5
+    echo ${blank} | tee $(ls /sys/class/graphics/fb*/blank)
+done
+
 for fb in /sys/class/graphics/fb[0-9];do
 	fb=$(basename ${fb})
-	echo 1 > /sys/class/graphics/${fb}/blank
-	sleep 1
-	gst-launch-1.0 videotestsrc ! fbdevsink device=/dev/${fb} &
-	sleep 1
-	echo 0 > /sys/class/graphics/${fb}/blank
+	disp_type=$(cat /sys/class/graphics/${fb}/fsl_disp_dev_property)
+	[[ ${disp_type} == 'overlay' ]] && continue
+	sleep .5
+	eval $(awk -F"," '$0="x="$1";y="$2' /sys/class/graphics/${fb}/virtual_size)
+	# gst-launch-1.0 videotestsrc ! "video/x-raw,width=${x},height=${y}" ! textoverlay text="'${fb}-${disp_type}'" ! fbdevsink device=/dev/${fb} &
+	gst-launch-1.0 videotestsrc ! "video/x-raw,width=${x},height=${y}" ! fbdevsink device=/dev/${fb} &
 done
